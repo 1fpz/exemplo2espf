0f925a8fc3aa3752eb7d1859581c944f421e2d24
Felipe <grungeskater777@gmail.com>
2026-03-27 09:53:43 -0300

    feat: Declarações e variáveis

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Seja bem vindo Dev")
+
+// DECLARACOES E VARIAVEIS
+// VAR, LET E CONST
+
+var nome="Fiap";
+console.log(nome);
+var nome="Dev";
+console.log(nome);
+
+if(true){
+    var avatar="Fiapudo"
+}
+
+// se fosse let nao deixaria acessar os dados de fora
+console.log(avatar);
